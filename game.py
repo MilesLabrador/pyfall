37d3a69d81de919b7ae86c39699d9b6e9ea3ef49
Miles <44444826+MilesLabrador@users.noreply.github.com>
2019-06-28 00:31:09 -0700

    Communication using socketio implemented and flask in order to detect newconnections, join game request, and create game request with data relayed by socketio
    Socketio will report the fields "name", "time", and "minimumvotes" when a new game creation is submitted, and "name" and "gamekey" when a join game request is submitted. I had to make the name fields different for the two forms, since they couldn't share the same name of "name" without causing errors, therefore I made one newgamename and joingamename, even though they serve the same purpose

--- game.py
+++ game.py
@@ -11,5 +11,20 @@ def index():
     """Serve index html"""
     print('hello!')
     return render_template('index.html')
+
+@socketio.on('newgame')
+def create_game(json, methods=['GET','POST']):
+    print('recieved new game request: '+str(json))
+    socketio.emit('')
+
+@socketio.on('joingame')
+def join_game(json, methods=['GET','POST']):
+    print('recieved join game request: '+str(json))
+    socketio.emit('')
+
+
+@socketio.on('newconnection')
+def new_connection(json, methods=['GET','POST']):
+    print('new connection: '+ str(json))
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
