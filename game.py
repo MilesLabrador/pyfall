02da5323028ab886ba337da57c1b1bba72d05eb1
Miles <44444826+MilesLabrador@users.noreply.github.com>
2019-07-10 23:08:06 -0700

    Beginning of working game and website. Added unique socket tracking with request.sid, ability to start game from waiting room,
    This push brings a proof of concept of the original spyfall game. With the addition of the tracking of individual sockets, we are able to assign different sockets different roles that are hidden to other players with other unique sockets.

--- game.py
+++ game.py
@@ -1,24 +1,26 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 from locations import locations
 import numpy as np
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-gamerooms = {} # initialize dictionary that keeps track of all gamerooms open and 
+gamerooms = {} # initialize dictionary that keeps track of all gamerooms open and
+sockets_gamekey = {}
 class spyfallround():
     duration = 480 # default duration is 8 minutes
     players = [] # initialize player list
     players_roles = {} # initialize player roles dictionary
-    def __init__(self, gamekey, duration, host):
+    def __init__(self, gamekey, duration, host, host_socket):
         self.duration = duration
         self.location = np.random.choice(list(locations.keys()))
         self.gamekey = gamekey
-        self.players = [host]
+        self.players = {host:host_socket}
+        self.started = False
     def assign_roles(self):
         """Randomly assign roles to players of the game with minimal duplicates"""
-        players_list = self.players # grab all players
+        players_list = list(self.players.keys()) # grab all players
         roles_list = locations[self.location] #list of roles from our locations dictionary
         self.spy = np.random.choice(players_list) #randomly choose spy
         if len(players_list)-1 > len(roles_list): #If there are more non-spies than there are non-spy roles, extend the roles list
@@ -39,6 +41,7 @@ class spyfallround():
                 n+=1
             else:
                 self.players_roles[player] = 'Spy'
+        self.started = True
 
 @app.route('/')
 def index():
@@ -47,40 +50,66 @@ def index():
     return render_template('index.html')
 
 @app.route('/<gamekey>')
-def gameroom(gamekey):
-    print(gamekey)
-    return 'your gamekey is: '+ gamekey
+def gameroom(gamekey, methods=['GET','POST']):
+    #print(gamekey)
+    #print('socketid gamekeypage',request.sid)
+    return render_template('index.html')
+    #return 'your gamekey is: '+ gamekey
 
 @socketio.on('newgame')
 def create_game(json, methods=['GET','POST']):
     """Initializes an instance of the the class spyfallround, which will serve roles and game parameters"""
+    global gamerooms
+    global sockets_gamekey
     print('recieved new game request: '+str(json))
     createinfo_dict = dict(json) # convert json to dict to parse data from
-    gamerooms[createinfo_dict['gamekey']] = spyfallround(createinfo_dict['gamekey'], createinfo_dict['duration'], createinfo_dict['name']) # Create instance of spyfallround() with the socketio emitted user inputs from the create game page
-    print(gamerooms)
+    socket_id = request.sid
+    sockets_gamekey[socket_id] = createinfo_dict['gamekey']
+    gamerooms[createinfo_dict['gamekey']] = spyfallround(createinfo_dict['gamekey'], createinfo_dict['duration'], createinfo_dict['name'], socket_id) # Create instance of spyfallround() with the socketio emitted user inputs from the create game page
+    print('players_in_game', gamerooms[createinfo_dict['gamekey']].players)
+    print('TOTAL GAMEROOMS:', gamerooms)
     socketio.emit('')
 
 @socketio.on('joingame')
 def join_game(json, methods=['GET','POST']):
     """check if gamekey is valid, if so add player to appropriate game"""
-    
+    global gamerooms
+    global sockets_gamekey
     print('recieved join game request: '+str(json))
+    socket_id = request.sid
     joininfo_dict = dict(json)
     if joininfo_dict['gamekey'] in gamerooms.keys():
-      print('User has a valid gamekey, let him join!')
-      gamerooms[joininfo_dict['gamekey']].players.append(joininfo_dict['name'])
-      print(gamerooms[joininfo_dict['gamekey']].players)
+      if joininfo_dict['name'] not in gamerooms[joininfo_dict['gamekey']].players:
+          sockets_gamekey[socket_id] = joininfo_dict['gamekey']
+          print('Valid Gamekey, Valid Name, JOIN')
+          gamerooms[joininfo_dict['gamekey']].players[joininfo_dict['name']] = socket_id
+          print('total_players_in_game', gamerooms[joininfo_dict['gamekey']].players)
+          emit('joingameserver', room=socket_id)
+      else:
+        print('Valid Gamekey, but Name taken, DONT JOIN')
     else:
-      print("Invalid gameroom key, nothing to join")
-    socket_id = request.sid
-    print('socket_id', socket_id)
+      print("Invalid Gamekey, DONT JOIN")
     socketio.emit('')
-
+@socketio.on('startgame')
+def startgame():
+    global gamerooms
+    global sockets_gamekey
+    print('startgame')
+    #print('players_in_game',)
+    socket_id = request.sid
+    gamerooms[sockets_gamekey[socket_id]].started = True
+    gamerooms[sockets_gamekey[socket_id]].assign_roles()
+    for player in gamerooms[sockets_gamekey[socket_id]].players.keys():
+        print(player, socket_id)
+        print('STORED SOCKET', gamerooms[sockets_gamekey[socket_id]].players[player])
+        print('ROLE', gamerooms[sockets_gamekey[socket_id]].players_roles[player])
+        emit('startgame', gamerooms[sockets_gamekey[socket_id]].players_roles[player], room=gamerooms[sockets_gamekey[socket_id]].players[player])
+    print('total_players FINAL', gamerooms[sockets_gamekey[socket_id]].players)
 
 @socketio.on('newconnection')
 def new_connection(json, methods=['GET','POST']):
     print('new connection: '+ str(json))
     socket_id = request.sid
-    print('socketid', socket_id)
+    print('socket_id',request.sid)
 if __name__ == '__main__':
     socketio.run(app, debug=True)
